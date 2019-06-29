c2dd070af461036190a0794b63c15c971fb470bf
jairojuunior <jairodasilvafreitasjunior@hotmail.com>
2019-06-29 11:47:52 -0300

    Created classes and UML diagram

---
+++ Peer/src/peer/Gossiper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peer;
+
+/**
+ *
+ * @author guest-tdld1a
+ */
+
+/*
+PARAMETROS DO CONSTRUTOR
+-TEMPO DE ENVIO
+-TABELA DE ENVIO (AQUI SE FOR GOSSIP DO PROPRIO PEER PASSA A TABELA DELE, 
+    SENAO A TABELA DA REDE QUE O PEER POSSUI)
+-ENDERECO DOS PEERS DA REDE
+-
+*/
+public class Gossiper {
+    
+}
